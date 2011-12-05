1d609ddde607a5023d99ebdc08b430fa7a97db75
Andrew <andrew@fisherboy.co.nz>
2011-12-05 16:40:10 +1300

    Cleaned up the file

--- inject.js
+++ inject.js
@@ -1,10 +1,22 @@
 
 
 
-//global vars
+//----- global vars -----
+//menu currently displaying or not
 var TMENU_ACTIVE = false;
+//menu loaded (stops the refreshing)
+var TMENU_LOADED = false;
+//html id of the menu
+var TMENU_ID_HMTL = 'tui-menu';
+//jquery formatted id
+var TMENU_ID = '#' + TMENU_ID_HMTL;
+
+//twitter anywhere api link
+var TWITTER_ANYWHERE_HREF = "http://platform.twitter.com/anywhere.js?id=IJUb6CuxDlb9CeDVwAcQ&v=1";
 
 
+
+//----- start script -----
 
 dlog("checking for required params");
 
@@ -14,13 +26,12 @@ if(params["name"] && params["type"] && params["type"] == "gene")
 {
 	dlog("valid params");
 	
-	
 	//jquery init
 	 $(document).ready(function() {
 	 
 		dlog("linking twitter api");
 		//load js
-		$.getScript("http://platform.twitter.com/anywhere.js?id=IJUb6CuxDlb9CeDVwAcQ&v=1");
+		$.getScript(TWITTER_ANYWHERE_HREF);
 	 
 	   	createTuiMenu();
 		
@@ -35,7 +46,7 @@ function createTuiMenu()
 	dlog("creating tui menu");
 	
 	//using jquery append new element to body
-	$('body').append('<div id="tui-menu"></div>');
+	$('body').append('<div id="' + TMENU_ID_HMTL + '"></div>');
 
 	
 	//scrolling event
@@ -48,32 +59,29 @@ function createTuiMenu()
 	});
 	
 	//click listener
-	$('#tui-menu').click(function(event) {
+	$(TMENU_ID).click(function(event) {
 		 menuClicked();
 	});
 	
-	//first init
+	//init
 	refreshTuiMenu();
-	
-	
-	
-
 }
 
 
-var TMENU_LOADED = false;
 
 
+//refresh function called everytime page updates
 function refreshTuiMenu()
 {
-	//set padding 
-	$('#tui-menu').css('padding', '25px 25px 25px 25px');
+	//set padding (to make it look good
+	$(TMENU_ID).css('padding', '25px 25px 25px 25px');
 
 	if(TMENU_ACTIVE && !TMENU_LOADED)
 	{
-		$('#tui-menu').html('<p> test data </p> <div id="tbox"> </div> <p> <a id="tui-tweet" href="#">Like this [data]</a>');
+		$(TMENU_ID).html('<p> <a id="tui-tweet" href="#">Like this [data]</a> </p>');
 		TMENU_LOADED = true;
 		
+		//popup window for twitter
 		$('#tui-tweet').click(function() {
 			var newwindow=window.open('https://twitter.com/intent/tweet?text=%3Cmsg%20here%3E%20%23tui%20tui%3AI%20tui%3Alike%20tairg%3AAT1G01040.1',
 			'Post To Twitter','height=350,width=650');
@@ -83,17 +91,14 @@ function refreshTuiMenu()
 	}
 	else if(!TMENU_ACTIVE)
 	{
-		$('#tui-menu').html('<p>Click Here</p>');
+		$(TMENU_ID).html('<p>Click Here</p>');
 		TMENU_LOADED = false;
 	}
 
-	//grab the #content (as all gbrowse sites have this)
-	//				 center 
-	var data = (($(document).width()/2) + (($('#content').width())/2)) - $('#tui-menu').width()/2 + 'px' ;
+	//grab the #content 
+	var data = (($(document).width()/2) + (($('#content').width())/2)) - $(TMENU_ID).width()/2 + 'px' ;
 
-	
-	
-	$('#tui-menu').css({
+	$(TMENU_ID).css({
 		position: 'fixed',
 		top: 'auto',
 		bottom: '0px',
@@ -101,18 +106,20 @@ function refreshTuiMenu()
 	});
 	
 	//set a colour
-	$('#tui-menu').css('background-color', '#DEB887');
+	$(TMENU_ID).css('background-color', '#DEB887');
 }
 
 
 
-
+//called when menu is clicked on
 function menuClicked()
 {
 	TMENU_ACTIVE = !TMENU_ACTIVE;
 	refreshTuiMenu();
 }
 
+
+//debug log for chrome
 function dlog(text)
 {
 	console.log("#tui: " + text);
