05c0284af6639293bcdd3b3ffa86129424a0273a
Andrew <andrew@fisherboy.co.nz>
2011-12-09 10:26:56 +1300

    Added Identi.ca messaging service to the ServiceProvider

--- tui.js
+++ tui.js
@@ -107,7 +107,7 @@ var TUI = {
 		var name = TUI.getTuiObjectName();
 		var id = TUI.getTuiMeta(TUI.META_TUI_ID);
 		
-		return sprintf(TUI_LIKE_FORMAT, name, id);
+		return sprintf(TUI.TUI_LIKE_FORMAT, name, id);
 	},
 	
 	
@@ -159,17 +159,15 @@ var TUI = {
 		{
 			dlog("Valid TUI Page");
 			
-			//ensure we have all the libs loaded
-			TUI._init();
-			
 			//pass loading to JQuery 
 			$(document).ready(function() {
+			
+				//ensure we have all the libs loaded
+				TUI._init();
 				//adds meta-data to the page
 				TUI._encodeTuiMetaData();
 				//embeds a like menu to the page
 				TUI._createLikeMenu();
-				
-				alert(TUI.createTuiLike());
 			});
 			
 		}
@@ -190,6 +188,8 @@ var TUIServiceProvider = {
 	
 	//url to open when using twitter
 	TWITTER_POPUP_TWEET_URL: 'https://twitter.com/intent/tweet?text=',
+	//url to open when using identi.ca (NOTE: if user is not logged in then this will not work)
+	IDENTICA_POPUP_NOTICE_URL: 'http://identi.ca/index.php?action=newnotice&status_textarea=',
 	
 	// Current set provider
 	provider: 0,
@@ -211,8 +211,8 @@ var TUIServiceProvider = {
 	
 		switch(provider)
 		{
-			case SP_Twitter: TUIServiceProvider._postMessageTwitter(message); break;
-			case SP_Identica: TUIServiceProvider._postMessageIdentica(message); break;
+			case TUIServiceProvider.SP_Twitter: TUIServiceProvider._postMessageTwitter(message); break;
+			case TUIServiceProvider.SP_Identica: TUIServiceProvider._postMessageIdentica(message); break;
 			default: throw('No Service Provider has been set!');
 		}
 	},
@@ -222,15 +222,15 @@ var TUIServiceProvider = {
 	
 	//posts a message to twitter
 	_postMessageTwitter: function(message) {
-		var newwindow=window.open(TWITTER_POPUP_TWEET_URL + encodeURIComponent(message) ,
+		var newwindow=window.open(TUIServiceProvider.TWITTER_POPUP_TWEET_URL + encodeURIComponent(message) ,
 		'Post To Twitter','height=350,width=650');
 	},
 	
 	
 	//posts a message to identica
 	_postMessageIdentica: function(message) {
-	
-		//TODO
+		var newwindow=window.open(TUIServiceProvider.IDENTICA_POPUP_NOTICE_URL + encodeURIComponent(message) ,
+		'Post To Identi.ca','height=350,width=650');
 	}
 }
 
