edbe2f67b2e3cab552127905add70a31547d423f
Mukti Prabhakar <mukti.prabhakar@yahoo.com>
2011-12-08 15:16:22 +1300

    Displaying the like count

--- inject.js
+++ inject.js
@@ -14,12 +14,13 @@ var TUI_LIKE = false;
 var TWITTER_ANYWHERE_HREF = "http://platform.twitter.com/anywhere.js?id=IJUb6CuxDlb9CeDVwAcQ&v=1";
 //twitter popup tweet 
 var TWITTER_POPUP_TWEET = 'https://twitter.com/intent/tweet?text=';
+var numberOfLikes;
 
-
+var foundEl;
 //TUI like message format 
 //{0} = tui id (eg: tairg)
 //{1} = object id (eg: AT1G01040.1)
-var TUI_LIKE_FORMAT = '<msg here> #tui tui:I tui:like %s:%s';
+var TUI_LIKE_FORMAT = '<msg here> #tui :I :like %s:%s';
 
 
 //----- start script -----
@@ -38,22 +39,28 @@ if(params["name"] && params["type"] && params["type"] == "gene")
 	
 	//jquery init
 	 $(document).ready(function() {
-	 
+		
 		dlog('Document ready');
 	 
 		dlog('finding "gene model" tag"');
-		var foundEl = $('td').find(":contains('Gene Model: " + params["name"] + "')")[0];
-		
+		foundEl = $('td').find(":contains('Gene Model: " + params["name"] + "')")[0];
+		//$(document).everyTime('5s',getLikeCount(foundEl,false),'0');
 		if(foundEl)
 		{
 			dlog('Found element')
+			//query the number of likes on that gene
+			dlog('getting the number of likes of this gene');
+			 getLikeCount(foundEl,true);
+			dlog('TESTING');
+		
+			
 			
 			dlog("linking twitter api");
 			//load js
 			$.getScript(TWITTER_ANYWHERE_HREF);
-		 
+			
 			//createTuiMenu();
-			createTuiLike(foundEl);
+			
 			
 		}
 		else dlog('No element found');
@@ -66,16 +73,18 @@ else dlog("params not valid");
 var LIKE_IMG_EL = '<img src="http://teambravo.media.officelive.com/images/463px-Symbol_thumbs_up.svg.png" width="12" height="18" />';
 
 //appends a tui like button to the top of the page
-function createTuiLike(element)
+function createTuiLike(element,numberOfLikes)
 {
 	dlog('creating tui like element');
-
-	$(element).append(' <b><a id="tui-like-link">Like ' + LIKE_IMG_EL + '</a></b>');
+	
+	$(element).append(' <b><a id="tui-like-link">Like ' + LIKE_IMG_EL +' '+numberOfLikes +'</a></b>');
 	$('#tui-like-link').click(function () {
 		//alert('You like: ' + params["name"]);
 		tuiTweetPopup();
+		
 	});
-
+	
+		
 }
 
 
@@ -112,12 +121,18 @@ function tuiTweetPopup()
 {	
 	var tui_id = 'tairg';
 	var obj_id = params["name"];
-
-	newwindow=window.open(TWITTER_POPUP_TWEET + encodeURIComponent(sprintf(TUI_LIKE_FORMAT, tui_id, obj_id)) ,
+	var newwindow=window.open(TWITTER_POPUP_TWEET + encodeURIComponent(sprintf(TUI_LIKE_FORMAT, tui_id, obj_id)) ,
 				'Post To Twitter','height=350,width=650');
+	
+	
 }
 
-
+function getNewLikeCount()
+{
+	getLikeCount(foundEl,false);
+	dlog('Like count after liking: '+ numberOfLikes);
+	$('tui-like-link').append('Like ' + LIKE_IMG_EL +' '+numberOfLikes );
+}
 
 //refresh function called everytime page updates
 function refreshTuiMenu()
@@ -189,3 +204,31 @@ function getUrlVars() {
 	});
 	return vars;
 }
+var bLike;
+function getLikeCount(foundEl,bLike)
+{
+	var obj_id = params["name"];
+	dlog('gene name'+obj_id);
+	var url='http://search.twitter.com/search.json?&q=';
+	
+	var query;
+	
+	
+		query="#tui :I :like tairg:"+obj_id;
+		dlog('The query is ' + query);
+		$.getJSON(url+encodeURIComponent(query),function(json)
+		{
+			
+			$.each(json.results,function(i,tweet)
+			{
+			  // $("#results").append('<p><img src="'+tweet.profile_image_url+'" widt="48" height="48" />'+tweet.text+'</p>');
+			   numberOfLikes = i+1; //starts with 0
+			});
+			dlog('numberOfLikes :'+numberOfLikes);
+			if(bLike)
+			{
+				createTuiLike(foundEl,numberOfLikes);
+			}
+		});
+		
+	}
