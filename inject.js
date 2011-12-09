2af8094386fcaaee70c8abe113dec88328416b19
Mukti Prabhakar <mukti.prabhakar@yahoo.com>
2011-12-09 14:08:15 +1300

    removed 'users' array - not required

--- inject.js
+++ inject.js
@@ -23,19 +23,33 @@ var foundEl;
 TUI.load();
 
 
+//TUI like message format 
+//{0} = tui id (eg: tairg)
+//{1} = object id (eg: AT1G01040.1)
+var TUI_LIKE_FORMAT = '<msg here> #tui :I :like %s:%s';
+
 
 //----- start script -----
 
+dlog("checking for required params");
+
+var params = getUrlVars();
+
 //this ensures the params are correct 
-if(TUI.isValidTuiPage())
-{	
+if(params["name"] && params["type"] && params["type"] == "gene")
+{
+	dlog("valid params");
+	
+	//now we need to check for a valid page
+	
+	
 	//jquery init
 	 $(document).ready(function() {
 		
 		dlog('Document ready');
 	 
 		dlog('finding "gene model" tag"');
-		foundEl = $('td').find(":contains('Gene Model: " + TUI.getCurrentId() + "')")[0];
+		foundEl = $('td').find(":contains('Gene Model: " + params["name"] + "')")[0];
 		var refreshLikeCount = setInterval(getNewLikeCount,1000);
 		if(foundEl)
 		{
@@ -59,6 +73,7 @@ if(TUI.isValidTuiPage())
 		
 	 });
 }
+else dlog("params not valid");
 
 //image element to a 'like' symbol
 var LIKE_IMG_EL = '<img src="http://teambravo.media.officelive.com/images/463px-Symbol_thumbs_up.svg.png" width="12" height="18" />';
@@ -70,6 +85,7 @@ function createTuiLike(element,numberOfLikes)
 	
 	$(element).append(' <b><a id="tui-like-link">Like ' + LIKE_IMG_EL +' '+numberOfLikes +'</a></b>');
 	$('#tui-like-link').click(function () {
+		//alert('You like: ' + params["name"]);
 		tuiTweetPopup();
 		
 	});
@@ -86,14 +102,35 @@ function createTuiMenu()
 	//using jquery append new element to body
 	$('body').append('<div id="' + TMENU_ID_HMTL + '"></div>');
 
+	
+	//scrolling event
+	$(window).scroll(function(event) {
+		refreshTuiMenu();
+	});
+	//when window is resized
+	$(window).resize(function(event) {
+		refreshTuiMenu();
+	});
+	
+	//click listener
+	$(TMENU_ID).click(function(event) {
+		 menuClicked();
+	});
+	
+	//init
+	refreshTuiMenu();
 }
 
 
 
 function tuiTweetPopup()
 {	
-    TUIServiceProvider.setProvider(TUIServiceProvider.SP_Twitter);
-    TUIServiceProvider.postMessage(TUI.createTuiLike());
+	var tui_id = 'tairg';
+	var obj_id = params["name"];
+	var newwindow=window.open(TWITTER_POPUP_TWEET + encodeURIComponent(sprintf(TUI_LIKE_FORMAT, tui_id, obj_id)) ,
+				'Post To Twitter','height=350,width=650');
+	
+	
 }
 
 function getNewLikeCount()
@@ -103,39 +140,100 @@ function getNewLikeCount()
 	createTuiLike(foundEl,numberOfLikes);
 }
 
+//refresh function called everytime page updates
+function refreshTuiMenu()
+{
+	//set padding (to make it look good
+	$(TMENU_ID).css('padding', '25px 25px 25px 25px');
 
+	if(TMENU_ACTIVE && !TMENU_LOADED)
+	{
+		$(TMENU_ID).html('<p> <a id="tui-tweet">Like '+ params["name"] + '</a>');
+		TMENU_LOADED = true;
+		
+		if(!TUI_LIKE)
+		{
+			
+		//popup window for twitter
+			$('#tui-tweet').click(function() {
+				TUI_LIKE = !TUI_LIKE;
+				tuiTweetPopup();
+				//if (window.focus) {newwindow.focus()}
+			});
+		}
+		else
+		{
+			$(TMENU_ID).html('<p> <a id="tui-tweet">You Like '+ params["name"] + '</a>');
+		}
+	}
+	else if(!TMENU_ACTIVE)
+	{
+		$(TMENU_ID).html('<p>Click Here</p>');
+		TMENU_LOADED = false;
+	}
 
+	//grab the #content 
+	var data = (($(document).width()/2) + (($('#content').width())/2)) - $(TMENU_ID).width()/2 + 'px' ;
+
+	$(TMENU_ID).css({
+		position: 'fixed',
+		top: 'auto',
+		bottom: '0px',
+		left: data
+	});
+	
+	//set a colour
+	$(TMENU_ID).css('background-color', '#DEB887');
+}
+
+//called when menu is clicked on
+function menuClicked()
+{
+	TMENU_ACTIVE = !TMENU_ACTIVE;
+	refreshTuiMenu();
+}
+
+//debug log for chrome
+function dlog(text)
+{
+	console.log("#tui: " + text);
+}
+
+//source: http://papermashup.com/read-url-get-variables-withjavascript/
+function getUrlVars() {
+	var vars = {};
+	var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+		vars[key] = value;
+	});
+	return vars;
+}
 var bLike; //boolean like,when the user has clicked like
 function getLikeCount(foundEl,bLike)
 {
-	var obj_id = TUI.getCurrentId();
+	var obj_id = params["name"];
 	var url='http://search.twitter.com/search.json?q=';
 	var query;
-	var users = new Array();
-	query=TUI.createTuiLike();
+	query="#tui :I :like tairg:"+obj_id;
 		
 	$.getJSON(url+encodeURIComponent(query),function(json)
 	{
-		$.each(json.results,function(i,tweet)
-		{
-		  users.push(tweet.from_user);
-		});
 		numberOfLikes = 0;
 		var actualUsers =  new Array();	
-			
-		for(var j = 0 ; j < users.length;j++)
+		$.each(json.results,function(i,tweet)
 		{
-			var user = users.slice(j,j+1);
-			if(j>0)
+			if(i>0)
 			{
+				var user = tweet.from_user;
 				checkUser(user,actualUsers);
 			}
 			else
 			{
-				actualUsers.push(users.slice(j,j+1));	
+				actualUsers.push(tweet.from_user);	
 				numberOfLikes = 1;
 			}
-		}
+		 
+		});
+					
 		if(bLike)
 		{
 			createTuiLike(foundEl,numberOfLikes);
