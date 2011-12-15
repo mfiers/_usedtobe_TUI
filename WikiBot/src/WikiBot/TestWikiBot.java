f4db024ef1e48e15fbc25646f1d0bfb028807e6a
mpra289 <mukti.prabhakar@yahoo.com>
2011-12-16 09:59:36 +1300

    Update WikiBot/src/WikiBot/TestWikiBot.java

--- WikiBot/src/WikiBot/TestWikiBot.java
+++ WikiBot/src/WikiBot/TestWikiBot.java
@@ -52,11 +52,10 @@ public class TestWikiBot {
         userList = ap.getUserList();
     }
 /** adds username **/
-<<<<<<< HEAD
+
     private void addUsername() {
-=======
+
     public void addUsername() {
->>>>>>> fc8a0060f29b12111e965ee1cb0d19d51c0caa16
         int i = 0;
         Iterator iterator = userList.iterator();
         while (iterator.hasNext()) {
