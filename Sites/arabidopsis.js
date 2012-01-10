f82262d4d6cac12e944358dc4c685478bf7e2eb6
Andrew <andrew@fisherboy.co.nz>
2012-01-11 12:58:53 +1300

    TAIR plugin finished

--- Sites/arabidopsis.js
+++ Sites/arabidopsis.js
@@ -7,17 +7,76 @@ if(location.href.indexOf("http://www.arabidopsis.org/servlets/TairObject?") == 0
 
     $(document).bind('tuiLoaded', function() 
     {
-        loadUI();
+        loadTair();
     });
 }
 
 
 
 
-function loadUI()
+function loadTair()
 {
-    console.log("Loading UI");
-    TUI.setTuiMeta(TUI.META_TUI_ID, "id_test");
-    TUI.setTuiMeta(TUI.META_TUI_TYPE, "type_test");
-    TUI.setTuiMeta(TUI.META_TUI_ID_PREFIX, "prefix_test");
+
+    //load all the variables
+    var params = getUrlVars();
+
+    //----------------------------------
+    //get the type
+    TUI.setTuiMeta(TUI.META_TUI_TYPE, getUrlVars()["type"]);
+
+    var idPfx; //undefined if nothing found
+
+    switch(TUI.getTuiMeta(TUI.META_TUI_TYPE))
+    {
+        case "gene" : idPfx = "tairg"; break;
+        case "locus" : idPfx = "tairl"; break;
+        case "aa_sequence" : idPfx = "tairp"; break;
+    }
+    
+    //----------------------------------
+    //set the id prefix
+    TUI.setTuiMeta(TUI.META_TUI_ID_PREFIX, idPfx);
+    
+
+    //set the  id
+    var el = getElementToInject();        
+    var data = el.innerText.split(": ")[1];
+    data = $.trim(data);  
+    TUI.setTuiMeta(TUI.META_TUI_ID, data);
+    
+    
+    //inject the like UI
+    TUIView.injectLikeDisplay(getElementToInject());
+}
+
+
+//source: http://papermashup.com/read-url-get-variables-withjavascript/
+function getUrlVars() {
+	var vars = {};
+	var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+		vars[key] = value;
+	});
+	return vars;
+}
+
+
+//gets the title cell
+function getElementToInject()
+{
+    var el;
+    //first need to see what type of page this is
+    switch(TUI.getTuiMeta(TUI.META_TUI_TYPE))
+    {
+
+        case "gene" : el = findTargetId('Gene Model: '); break;
+        case "locus": el = findTargetId('Locus: '); break;
+        case "aa_sequence": el = findTargetId('Protein: '); break;
+    }
+    return el;
+}
+
+//finds the cell with the specified contents
+function findTargetId(tdContent) 
+{
+    return $('td').find(":contains('" + tdContent + "')")[0];
 }
