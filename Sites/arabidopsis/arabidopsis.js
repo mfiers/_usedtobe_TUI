e0dc46c520516c082cd1a952029e1e7bfd6f00f4
Andrew <andrew@fisherboy.co.nz>
2012-01-11 11:03:16 +1300

    Started new plugin directed at TAIR

--- Sites/arabidopsis/arabidopsis.js
+++ Sites/arabidopsis/arabidopsis.js
@@ -1,5 +1,23 @@
 //arabidopsis.org TUI script
 
-TUI.setTuiMeta(TUI.META_TUI_ID, "id_test");
-TUI.setTuiMeta(TUI.META_TUI_TYPE, "type_test");
-TUI.setTuiMeta(TUI.META_TUI_ID_PREFIX, "prefix_test");
+//only works on this valid site
+if(location.href.indexOf("http://www.arabidopsis.org/servlets/TairObject?") == 0)
+{
+    console.log("VALID");
+
+    $(document).bind('tuiLoaded', function() 
+    {
+        loadUI();
+    });
+}
+
+
+
+
+function loadUI()
+{
+    console.log("Loading UI");
+    TUI.setTuiMeta(TUI.META_TUI_ID, "id_test");
+    TUI.setTuiMeta(TUI.META_TUI_TYPE, "type_test");
+    TUI.setTuiMeta(TUI.META_TUI_ID_PREFIX, "prefix_test");
+}
