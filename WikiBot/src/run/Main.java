1d51f47199b145513ecf0d6125759bb917681eec
Andrew <andrew@fisherboy.co.nz>
2012-01-19 16:12:39 +1300

    Added Exception handling

--- WikiBot/src/run/Main.java
+++ WikiBot/src/run/Main.java
@@ -6,6 +6,8 @@ package run;
 
 import AtomParser.newParser;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -21,49 +23,58 @@ public class Main {
         {
             if(scan.hasNextLine())
             {
-                String line = scan.nextLine();
-                String[] splt = line.split("\\s");
-                
-                //username
-                final String user = splt[0];
-                
-                //build the message
-                String messageBuilder = "";
-                for(int l = 1; l< splt.length; l++)
+                try
                 {
-                    messageBuilder += splt[l] + " ";
-                }
-                messageBuilder = messageBuilder.trim();
-              //if the message is a like/dislike ,change the message to uppercase
-                if(messageBuilder.toLowerCase().contains("like") || messageBuilder.toLowerCase().contains("dislike"))
+                    String line = scan.nextLine();
+                    String[] splt = line.split("\\s");
+
+                    //username
+                    final String user = splt[0];
+
+                    //build the message
+                    String messageBuilder = "";
+                    for(int l = 1; l< splt.length; l++)
+                    {
+                        messageBuilder += splt[l] + " ";
+                    }
+                    messageBuilder = messageBuilder.trim();
+                  //if the message is a like/dislike ,change the message to uppercase
+                    if(messageBuilder.toLowerCase().contains("like") || messageBuilder.toLowerCase().contains("dislike"))
+                            {
+                                messageBuilder = messageBuilder.toUpperCase();
+                            }
+                    else        //else change selected parts of the message to uppercase
+                    {
+                        //free text is removed from the message
+                        messageBuilder = messageBuilder.substring(messageBuilder.indexOf("#tui"));
+                        String msgSplit[] = messageBuilder.split(" ");
+                        messageBuilder = "";
+                        for(int i = 0 ; i <3 ;i++)
                         {
-                            messageBuilder = messageBuilder.toUpperCase();
+                            msgSplit[i]= msgSplit[i].toUpperCase();
                         }
-                else        //else change selected parts of the message to uppercase
-                {
-                    //free text is removed from the message
-                    messageBuilder = messageBuilder.substring(messageBuilder.indexOf("#tui"));
-                    String msgSplit[] = messageBuilder.split(" ");
-                    messageBuilder = "";
-                    for(int i = 0 ; i <3 ;i++)
-                    {
-                        msgSplit[i]= msgSplit[i].toUpperCase();
+                        for(int i = 0 ; i <msgSplit.length ;i++)
+                        {
+                            messageBuilder = messageBuilder.concat(" "+msgSplit[i]);
+                        }
                     }
-                    for(int i = 0 ; i <msgSplit.length ;i++)
-                    {
-                        messageBuilder = messageBuilder.concat(" "+msgSplit[i]);
-                    }
+
+
+                    final String message = messageBuilder;
+
+                    new Thread( new Runnable() {
+                        @Override
+                        public void run() {
+                            newParser we = new newParser(user, message);
+                        }
+                    }).start();
+
                 }
-              
-        
-                final String message = messageBuilder;
-               
-                new Thread( new Runnable() {
-                    @Override
-                    public void run() {
-                        newParser we = new newParser(user, message);
-                    }
-                }).start();
+                catch(Exception ex)
+                {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, "Exception happened in main");
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
         }
     }
