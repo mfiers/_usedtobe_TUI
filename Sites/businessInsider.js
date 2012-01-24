f2b3441aec40aa94aedcb1bb684295b91d5b4804
Mukti Prabhakar <mukti.prabhakar@yahoo.com>
2012-01-25 09:22:46 +1300

    Formatted businessInsider

--- Sites/businessInsider.js
+++ Sites/businessInsider.js
@@ -1,30 +1,25 @@
-if(location.href.indexOf("http://articles.businessinsider.com") === 0 || location.href.indexOf("http://www.articles.businessinsider.com") === 0 )
-{
-    $(document).bind('tuiLoaded', function() 
-    {
+if(location.href.indexOf("http://articles.businessinsider.com")  === 0  || location.href.indexOf("http://www.articles.businessinsider.com")  === 0 ) {
+    $(document).bind('tuiLoaded', function()  {
         loadBusinessinsider();
     });
 }
-function loadBusinessinsider()
-{
-	 TUI.setTuiMeta(TUI.META_TUI_ID_PREFIX, "businessInsider");
-
+function loadBusinessinsider() {
+    TUI.setTuiMeta(TUI.META_TUI_ID_PREFIX, "businessInsider");
     //split the URL
-    var urlSplit = window.location.href.split("/");
-	//get the last split string ,contains the id and title
-	var id = urlSplit[urlSplit.length-1];
-	var articleId = id.split("_")[0];
-	if(isNumber(articleId))
-	{
-		TUI.setTuiMeta(TUI.META_TUI_ID, articleId);
-		
-		//find element to insert
-		var el = $('#mod-article-header').find('h1');
-		//inject Like and Display
-		TUIView.injectTitleChangeDisplay(el);
-		TUIView.injectLikeDisplay(el, true);
-	}
- }
-function isNumber(n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
+     var urlSplit  = window.location.href.split("/");
+    //get the last split string ,contains the id and title
+    	var id  = urlSplit[urlSplit.length - 1];
+    var articleId  = id.split("_")[0];
+    if(isNumber(articleId)) {
+        TUI.setTuiMeta(TUI.META_TUI_ID, articleId);
+        //find element to insert
+		var el  = $('#mod-article-header').find('h1');
+        //inject Like and Display
+        TUIView.injectTitleChangeDisplay(el);
+        TUIView.injectLikeDisplay(el, true);
+    }
 }
+function isNumber(n)  {
+    return  ! isNaN(parseFloat(n))  && isFinite(n);
+}
+
