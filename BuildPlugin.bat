bb9f6b291974ee6f69bf741e26f813b40614aab6
Andrew <andrew@fisherboy.co.nz>
2012-02-16 11:26:43 +1300

    Implemented chrome extension icon. Closes #7

--- BuildPlugin.bat
+++ BuildPlugin.bat
@@ -10,6 +10,9 @@ XCOPY Plugin\* ..\Plugin\* /S
 ECHO Copying script
 XCOPY Sites\stuff.js ..\Plugin\
 
+ECHO Copying image
+XCOPY tui.png ..\Plugin\
+
 ECHO -----------------------
 ECHO Make sure to copy the 
 ECHO script name into the 
